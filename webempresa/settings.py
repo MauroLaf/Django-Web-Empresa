15d42c8c221629c9d82d4275eb2d1609c1b9ff39
Mauro Lafuente <155067361+MauroLaf@users.noreply.github.com>
2025-05-17 12:44:08 +0200

    debug false and allowed host '*'

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d%l^i0)ob)*clyddieh68&zu(^@5_^wcln+0_(pvm-6n%1qao-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
